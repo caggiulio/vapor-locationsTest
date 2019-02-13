2f05b9127a3273c364753cb49883cd1717781d59
Nunzio Giulio Caggegi - Skylabs <giulio.caggegi@skylabs.it>
2019-02-13 10:01:58 +0100

    add nil for empty tripID

--- Sources/App/Controller/LocationController.swift
+++ Sources/App/Controller/LocationController.swift
@@ -30,7 +30,7 @@ final class LocationController: RouteCollection {
     }
     
     func postArray(_ request: Request, _ location: [Location])throws -> Future<[Location]> {
-        return location.map { Location(lat: $0.lat, lng: $0.lng, timestamp: $0.timestamp, speed: $0.speed, tripID: $0.tripID!).save(on: request) }
+        return location.map { Location(lat: $0.lat, lng: $0.lng, timestamp: $0.timestamp, speed: $0.speed, tripID: ($0.tripID ?? "nil")).save(on: request) }
             .flatten(on: request)
         /*return try request.content.decode([Location].self).map { locRequest in
             return .ok

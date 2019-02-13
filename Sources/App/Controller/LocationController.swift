0ec8a82e6cee606f2969d9e3c542101ed65c52c3
Nunzio Giulio Caggegi - Skylabs <giulio.caggegi@skylabs.it>
2019-02-13 09:40:07 +0100

    comments

--- Sources/App/Controller/LocationController.swift
+++ Sources/App/Controller/LocationController.swift
@@ -32,6 +32,9 @@ final class LocationController: RouteCollection {
     func postArray(_ request: Request, _ location: [Location])throws -> Future<[Location]> {
         return location.map { Location(lat: $0.lat, lng: $0.lng, timestamp: $0.timestamp, speed: $0.speed, tripID: $0.tripID!).save(on: request) }
             .flatten(on: request)
+        /*return try request.content.decode([Location].self).map { locRequest in
+            return .ok
+        }*/
     }
 }
 

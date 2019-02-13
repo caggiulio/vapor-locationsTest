eee10c1ed7b1486b1947b88b6a60ee7dbe9947a8
Nunzio Giulio Caggegi - Skylabs <giulio.caggegi@skylabs.it>
2019-02-13 08:38:53 +0100

    add api for post more locations

--- Sources/App/Controller/LocationController.swift
+++ Sources/App/Controller/LocationController.swift
@@ -14,6 +14,7 @@ final class LocationController: RouteCollection {
         
         locations.post(Location.self, use: create)
         locations.get(use: index)
+        locations.post([Location].self, use: postArray)
         //locations.get(User.parameter, use: show)
         //locations.patch(UserContent.self, at: User.parameter, use: update)
         //locations.delete(User.parameter, use: delete)
@@ -28,25 +29,10 @@ final class LocationController: RouteCollection {
         return Location.query(on: request).all()
     }
     
-    /*func show(_ request: Request)throws -> Future<Location> {
-        return try request.parameters.next(User.self)
+    func postArray(_ request: Request, _ location: [Location])throws -> Future<[Location]> {
+        return location.map { Location(lat: $0.lat, lng: $0.lng, timestamp: $0.timestamp, speed: $0.speed, tripID: $0.tripID!).save(on: request) }
+            .flatten(on: request)
     }
-    
-    func update(_ request: Request, _ body: UserContent)throws -> Future<Location> {
-        let user = try request.parameters.next(User.self)
-        return user.map(to: User.self, { user in
-            user.username = body.username ?? user.username
-            user.firstname = body.firstname ?? user.firstname
-            user.lastname = body.lastname ?? user.lastname
-            user.email = body.email ?? user.email
-            user.password = body.password ?? user.password
-            return user
-        }).update(on: request)
-    }
-    
-    func delete(_ request: Request)throws -> Future<HTTPStatus> {
-        return try request.parameters.next(Location.self).delete(on: request).transform(to: .noContent)
-    }*/
 }
 
 struct LocationContent: Content {
